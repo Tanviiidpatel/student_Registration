2dbae2ec6f70e0313aad0c1c3cb8dd23606a9baa
Tanviiidpatel <153188370+Tanviiidpatel@users.noreply.github.com>
2024-08-23 17:41:43 +0530

    Update Registration.js

--- scansafe_register/src/components/Registration.js
+++ scansafe_register/src/components/Registration.js
@@ -24,7 +24,7 @@ const RegistrationForm = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('student-registration-api1.vercel.app/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
