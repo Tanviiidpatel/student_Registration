80b72e21c10a48935d566a6127b25d011253a1c0
Tanviiidpatel <153188370+Tanviiidpatel@users.noreply.github.com>
2024-08-24 15:57:56 +0530

    Update Registration.js

--- scansafe_register/src/components/Registration.js
+++ scansafe_register/src/components/Registration.js
@@ -23,7 +23,7 @@ const RegistrationForm = () => {
     console.log('Submitting form data:', formData); 
 
     try {
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('https://scansafe-frontend.web.app/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
